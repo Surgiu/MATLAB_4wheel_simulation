3a158fc25ef676f4f473c2dabe809eb96d23c867
Surgiu <113337694+Surgiu@users.noreply.github.com>
2024-06-04 00:03:45 +0800

    validations

--- myconsole.m
+++ myconsole.m
@@ -4,13 +4,13 @@ figure;
 %x方向速度输入
 subplot(2,1,1);
 plot(result.vx_input.Data);
-xlabel('time');
+xlabel('time/s');
 ylabel('input vx');
 title('Input Velocities')
 %y方向速度输入
 subplot(2,1,2);
 plot(result.vy_input.Data);
-xlabel('time');
+xlabel('time/s');
 ylabel('input vy');
 
 
@@ -20,23 +20,23 @@ figure;
 %绘制小车理论运行轨迹
 subplot(2,2,1);
 plot(result.x_desired.Data,result.y_desired.Data);
-xlabel('Horizontal Displacement');
-ylabel('Vertical Displacement');
+xlabel('x');
+ylabel('y');
 title('Ground Truth');
 axis equal; % 使x和y轴比例相同，更适合观察轨迹
 grid on;
 %绘制小车仿真轨迹
 subplot(2,2,2);
 plot(result.x.Data, result.y.Data);
-xlabel('Horizontal Displacement');
-ylabel('Vertical Displacement');
+xlabel('x');
+ylabel('y');
 title('Generated Trajactory');
 axis equal; % 使x和y轴比例相同，更适合观察轨迹
 grid on;
 %绘制小车y方向速度变化趋势
 subplot(2,2,3);
 plot(result.vy)
-title('v_y')
+title('Vertical Velocity')
 ylabel('v_y')
 xlabel('time/s')
 %四个轮子的角速度
@@ -51,3 +51,12 @@ legend('w1','w2','w3','w4')
 xlabel('time/s')
 ylabel('w')
 hold off
+
+%计算停止位置的坐标
+x_end = result.x.Data(end);
+y_end = result.y.Data(end);
+%arg_end = atan(y_end/(x_end-1)) * 180 / pi;
+disp(["simulation end point:", x_end, y_end])
+
+%arg_truth = atan(sin(10)/(-cos(10)-1))* 180 / pi;
+disp(["ground truth: ", 1-cos(10), sin(10)])
